73586ecba8f0f18fa9dc91acefc22ac70991abc4
SonPham <phamngocsonl13.cla@gmail.com>
2019-05-15 05:25:07 +0700

    Sửa responsive
    Người đăng:Phạm Ngọc Sơn
    Nhiệm vụ: Sửa responsive cho trang web

--- VuiHocTiengAnh.js
+++ VuiHocTiengAnh.js
@@ -1,9 +1,12 @@
 
- $(document).ready(function(){
- 	$.replace(/jQuery4u/g,'jQuery4u FTW!');
- 	alert('gellpo');
- 	$.ajax('data.txt', function(data) {
-  
+$(document).ready(function(){
+	$.get('https://sonpham1602.github.io/VuiHocTiengAnhWeb/data/Units/data.txt', function(data) {
+		alert(data);
+		var o = new Option("option text", "value");
+/// jquerify the DOM object 'o' so we can use the html method
+$(o).html("option text");
+$(".test").append(o);
 }, 'text');
 
- });
+
+});
