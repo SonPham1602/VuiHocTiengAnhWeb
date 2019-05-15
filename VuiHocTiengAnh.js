789fc69f5cbd287dc4f63d1a6def7a056612a95f
SonPham <phamngocsonl13.cla@gmail.com>
2019-05-15 12:03:10 +0700

    Upload imges
    Người đăng: Phạm Ngọc Sơn
    Nhiệm vụ: Upload từ unit 1 tới 21

--- VuiHocTiengAnh.js
+++ VuiHocTiengAnh.js
@@ -1,7 +1,6 @@
 
 $(document).ready(function(){
 	$.get('https://sonpham1602.github.io/VuiHocTiengAnhWeb/data/Units/data.txt', function(data) {
-		alert(data);
 		var o = new Option("option text", "value");
 /// jquerify the DOM object 'o' so we can use the html method
 $(o).html("option text");
